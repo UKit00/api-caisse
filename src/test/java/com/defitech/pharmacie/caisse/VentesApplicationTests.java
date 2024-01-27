0c7496c29c1dd289c9a314cd04954b9570eed861
Usa De Kit <israelmaou@gmail.com>
2024-01-27 14:43:29 +0000

    Reinitialisation caisse

---
+++ src/test/java/com/defitech/pharmacie/caisse/VentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defitech.pharmacie.caisse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
